327ce4173fc543c0254ae0e3600deb0fd0a05749
Saket Srivastava <saket@srivastava.pw>
2019-09-29 11:36:11 +0530

    Adding filter tag and handling null values

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Netbox AWX inventory script
-A simple inventory script to populate data from netbox to AWX inventory.  s script is tested with Netbox v2.6.2
+A simple inventory script to populate data from netbox to AWX inventory.  This script is tested with Netbox v2.6.2
 
 ## Grouping and Host vars
 It groups hosts based on site, rack, platform, tenant, tag.
 host vars are populated from hosts config_context.
+
+## Filtering hosts
+You might not want to import all devices from netbox to AWX inventory.  For such cases, a list of tags to filter the hosts may be added to FILTER_TAGS variable.  If FILTER_TAGS list is empty, all devices will be imported
--- netbox-awx.py
+++ netbox-awx.py
@@ -9,6 +9,9 @@ URL = ''
 # Netbox API Token
 TOKEN = ''
 
+# AWX Filter Tags
+FILTER_TAGS = []
+
 headers = {
     'Accept': 'application/json ; indent=4',
     'Authorization': 'Token %s' % (TOKEN),
@@ -35,17 +38,31 @@ while api_url:
         hosts_list += api_output_data["results"]
         api_url = api_output_data["next"]
 
+# Filter hosts for AWX
+
+for i in hosts_list:
+    if FILTER_TAGS:
+        if any(item in FILTER_TAGS for item in i['tags']):
+            devices.append(i)
+    else:
+        devices.append(i)
+
 # Populate inventory
 
 for i in devices:
-    sites.setdefault(i['site']['slug'], {'hosts': []})['hosts'].append(i['name'])
-    racks.setdefault(i['rack']['name'], {'hosts': []})['hosts'].append(i['name'])
-    platforms.setdefault(i['platform']['slug'], {'hosts': []})['hosts'].append(i['name'])
-    tenants.setdefault(i['tenant']['slug'], {'hosts': []})['hosts'].append(i['name'])
-    for t in i['tags']:
-        tags.setdefault(t, {'hosts': []})['hosts'].append(i['name'])
-    if i['config_context']:
-        hostvars.setdefault('_meta', {'hostvars': {}})['hostvars'][i['name']] = i['config_context']
+    if i['name']:
+        if i['site']:
+            sites.setdefault(i['site']['slug'], {'hosts': []})['hosts'].append(i['name'])
+        if i['rack']:
+            racks.setdefault(i['rack']['name'], {'hosts': []})['hosts'].append(i['name'])
+        if i['platform']:
+            platforms.setdefault(i['platform']['slug'], {'hosts': []})['hosts'].append(i['name'])
+        if i['tenant']:
+            tenants.setdefault(i['tenant']['slug'], {'hosts': []})['hosts'].append(i['name'])
+        for t in i['tags']:
+            tags.setdefault(t, {'hosts': []})['hosts'].append(i['name'])
+        if i['config_context']:
+            hostvars.setdefault('_meta', {'hostvars': {}})['hostvars'][i['name']] = i['config_context']
 
 inventory.update(sites)
 inventory.update(racks)
